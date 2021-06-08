380a2906cd11923b7314a77d9f68af762897eca7
Jose Uribe <joseuribeh@gmail.com>
2021-06-08 01:14:07 -0500

    Levantar estado

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,60 +1,85 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-export class BadgeForm extends Component {
-    state = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        jobTitle: '',
-        twitter: ''
-      };
+class BadgeForm extends Component {
 
-    handleChange = (e) => {
-        this.setState({ 
-            [e.target.name]: e.target.value,
-        })
-    }
+  handleClick = e => {
+    console.log('Button clicked');
+  };
 
-    handleClick = (e) => {
-        console.log("Button clicked")
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('Form submitted');
+    console.log(this.state);
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("Form submitted");
-        console.log(this.state);
-    }
+  render() {
+    return (
+      <div>
+        <h1>New Attendant</h1>
 
-    render() {
-        return (
-            <div>
-                <h1>New Attendant</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <label>First Name</label>
-                        <input onChange={this.handleChange} className="form-control" type="text" name="firstName" value={this.state.firstName} />
-                    </div>
-                    <div className="form-group">
-                        <label>Last Name</label>
-                        <input onChange={this.handleChange} className="form-control" type="text" name="lastName" value={this.state.lastName} />
-                    </div>
-                    <div className="form-group">
-                        <label>Email</label>
-                        <input onChange={this.handleChange} className="form-control" type="email" name="email" value={this.state.email} />
-                    </div>
-                    <div className="form-group">
-                        <label>Job Title</label>
-                        <input onChange={this.handleChange} className="form-control" type="text" name="jobTitle" value={this.state.jobTitle} />
-                    </div>
-                    <div className="form-group">
-                        <label>Twitter</label>
-                        <input onChange={this.handleChange} className="form-control" type="text" name="twitter" value={this.state.twitter} />
-                    </div>
-                    <button onClick={this.handleClick} className="btn btn-primary">Save</button>
-                </form>
-            </div>
-        )
-    }
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>First Name</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="firstName"
+              value={this.props.formValues.firstName}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Last Name</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="lastName"
+              value={this.props.formValues.lastName}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="email"
+              name="email"
+              value={this.props.formValues.email}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Job Title</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="jobTitle"
+              value={this.props.formValues.jobTitle}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Twitter</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="twitter"
+              value={this.props.formValues.twitter}
+            />
+          </div>
+
+          <button onClick={this.handleClick} className="btn btn-primary">
+            Save
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default BadgeForm
+export default BadgeForm;
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,40 +1,63 @@
-import React from 'react';
-import Badge from '../components/Badge'
-import Navbar from '../components/Navbar'
-import BadgeForm from '../components/BadgeForm'
+import React, { Component } from 'react';
 
-import './styles/BadgeNew.css'
-import header from '../images/badge-header.svg'
+import './styles/BadgeNew.css';
+import header from '../images/badge-header.svg';
+import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
+import Navbar from '../components/Navbar';
 
+class BadgeNew extends Component {
+  state = {
+    form: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      jobTitle: '',
+      twitter: '',
+    },
+  };
 
-class BadgeNew extends React.Component {
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <div className="BadgeNew__hero">
-                    <img className="img-fluid" src={header} alt="Stars" />
-                </div>
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
 
-                <div className="container">
-                    <div className="row">
-                        <div className="col-6">
-                            <Badge 
-                            avatarUrl="http://www.gravatar.com/avatar/?d=identicon" 
-                            firstName="Jose" 
-                            lastName="Uribe" 
-                            jobTitle="FrontEnd Developer" 
-                            twitter="joseuribeh" />
-                        </div>
-                        
-                        <div className="col-6">
-                            <BadgeForm />
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                twitter={this.state.form.twitter}
+                jobTitle={this.state.form.jobTitle}
+                email={this.state.form.email}
+                avatarUrl="https://www.gravatar.com/avatar"
+              />
             </div>
-        )
-    }
+
+            <div className="col-6">
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default BadgeNew;
