c63ccf6da22f0b097dd8557c38e9c9a2bc7a99a0
Jose Uribe <joseuribeh@gmail.com>
2021-06-12 00:28:00 -0500

    Details Page Added

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import BadgeNew from '../pages/BadgeNew'
 import Badges from '../pages/Badges'
 import NotFound from '../pages/NotFound'
 import BadgeEdit from '../pages/BadgeEdit'
+import BadgeDetails from '../pages/BadgeDetailsContainer'
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/badges" component={Badges} />
                 <Route exact path="/badges/new" component={BadgeNew} />
+                <Route exact path="/badges/:badgeId" component={BadgeDetails} />
                 <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
                 <Route component={NotFound} />
             </Switch>
